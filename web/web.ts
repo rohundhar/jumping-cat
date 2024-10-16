e71f60e6b1539564f44d1d77253aabb6100b1e5a
Rohun Dhar <rohundhar@gmail.com>
2024-10-15 21:04:51 -0700

    updated web server

--- web/web.ts
+++ web/web.ts
@@ -1,13 +1,21 @@
-//@ts-nocheck
-const express = require('express');
+import * as path from 'path';
+import express from 'express';
+
+
+const dirName = `/Users/rohundhar/Desktop/Projects/jumping-cat`;
+const port = 8080;
 const app = express();
 
-app.get('/auth', (req, res) => {
+app.get('/auth', (req: any, res: any) => {
   const code = req.query.code;
   // Handle the authorization code here
   res.send('Authorization code received!');
 });
 
-app.listen(8080, () => {
-  console.log('Server listening on port 8080');
+console.log(path.join(dirName, 'Assets'));
+// // Serve static files from the 'Assets' directory
+app.use('/Assets', express.static(path.join(dirName, 'Assets')));
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
 });
