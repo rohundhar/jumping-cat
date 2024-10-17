60216235f6f057758f3da889c031a95efb84cc08
Rohun Dhar <rohundhar@gmail.com>
2024-10-17 13:52:33 -0700

    update git ignore

--- GDrive/types.ts
+++
@@ -1,7 +0,0 @@
-export enum MimeType {
-  JPG = 'image/jpeg',
-  MP4 = 'video/mp4',
-  QUICKTIME = 'video/quicktime',
-  HEIC = 'image/heif',
-  PNG = 'image/png'
-}
--- config.ts
+++
@@ -1,7 +0,0 @@
-import * as dotenv from 'dotenv';
-
-dotenv.config();
-
-export default {
-  mongoURI: process.env.MONGODB_URI
-}
